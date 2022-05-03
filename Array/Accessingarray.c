e812ac0f3381cb9237935869ebf48b21e6528251
Gautam2605sharma <91370975+Gautam2605sharma@users.noreply.github.com>
2022-05-03 23:35:23 +0530

    Accessing array

---
+++ Array/Accessingarray.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+int a[5] = {2, 3, 5, 7, 11};
+
+printf("%d\n",a[0]); // we are accessing 
+
+printf("%d\n",a[1]);
+
+printf("%d\n",a[2]);
+
+printf("%d\n",a[3]);
+
+printf("%d",a[4]);
+
+return 0;
+
+}
